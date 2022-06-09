7d8c9b9511ba9dd6a081c8863108b039eb0df819
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-09 10:50:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+    {id: 1, content: 'buy milk'},
+    {id: 2, content: 'play some games'},
+
+    ]
+  }
+
+  deleteTodo = (id) => {
+    console.log(id);
+  }
+  render() {
+    return (
+      <div className="App container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Todos.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Todos = ({ todos, deleteTodo }) => {
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return ( 
+                <div className="collection item" key={todo.id}> 
+                    <span>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p>You have no todo's left</p>
+    )
+  return (
+    <div className="todos collection">
+        {todoList}
+    </div>
+    )
+}
+
+export default Todos
